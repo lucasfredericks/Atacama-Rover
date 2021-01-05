b3a94b52b12ea95eacb8867bf363fc682fd0b97a
Lucas Fredericks <41305969+lucasfredericks@users.noreply.github.com>
2021-01-05 14:55:31 -0500

    Update block_reader.ino

--- block_reader/block_reader.ino
+++ block_reader/block_reader.ino
@@ -1,22 +1,32 @@
+/* To add:
+   wait for query from rover before sending a command (sleep the rover's xbee and wake up intermittently
+        to see if the reader board wants to send a command
+
+
+*/
+
+
 
 //define where your pins are
 int latchPin = 8;
 int dataPin = 10;
 int clockPin = 9;
 const byte interruptPin = 3;
+
 volatile boolean button;
-
-const int rows = 6;
-
-uint8_t mainQueue [rows] = {0, 0, 0, 0, 0, 0};
-uint8_t funcQueue [rows] = {0, 0, 0, 0, 0, 0};
-uint8_t rightCompare = 15; //binary 00001111
-boolean wait;
-boolean function;
-int mainCt;
-int funcCt;
 volatile int strokes;
 boolean debug = false;
+
+const int rows = 6;
+uint8_t mainQueue [rows] = {0, 0, 0, 0, 0, 0};
+uint8_t funcQueue [rows] = {0, 0, 0, 0, 0, 0};
+uint8_t comboQueue [12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int mainCt; //iterator for mainQueue
+int funcCt; //iterator for funcQueue
+int userInput; //Serial in -- should probably be a local variable
+boolean wait; // waiting for feedback from rover to send the next command
+boolean function; //tracks whether to send the next command from the function queue or the main queue
+boolean done; // are there still commands in the queue?
 
 String commandLookup[16] = {
   /* Each shift register reads one byte from two code readers of four bits (i.e. a nibble) each.
@@ -28,11 +38,11 @@ String commandLookup[16] = {
 
      Since we are taking orientation into account for some commands
      but not others, there will be redundancy (i.e. a FUNC can be rotated to W,E or S,N,
-     but will still resolve to the same command, but a Drive block can be rotated to N, E, S, or W, 
+     but will still resolve to the same command, but a Drive block can be rotated to N, E, S, or W,
      and each orientation will resolve to a unique directional command).
   */
 
-//char         bin    int   ascii   command
+  //char         bin    int   ascii   command
   " ",      // 0000,  0     32      undefined
   "n",      // 0001,  1     110     north
   "e",      // 0010,  2     101     east
@@ -62,31 +72,56 @@ void setup() {
   pinMode(dataPin, INPUT);
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPress, FALLING);
-  
+
   strokes = 0;
   wait = true;
   button = false;
   function = false;
+  clearQueue();
 }
 
-void buttonPress(){
-  detachInterrupt(digitalPinToInterrupt(interruptPin);
-  button = true;  
+void buttonPress() {
+  detachInterrupt(digitalPinToInterrupt(interruptPin));
+  button = true;
   strokes++;
   delay(20);
-  attachInterrupt(digitalPinToInterrupt(interruptPin);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPress, FALLING);
 }
 void loop() {
-  
-  if(button){
+
+  if (button) {
+    button = false;
     clearQueue();
-    shiftIn();      
+    shiftIn();
   }
-  if(!wait){
-  sendCommands();
+  if ((!wait) && ((!done || function))) {
+    sendCommand();
+  }
+
+  if (Serial.available()) { //see if the rover has sent a message
+    userInput = Serial.read();
+    if (userInput == 1) { // received
+      wait = true;
+    }
+
+    else if (userInput == 2) { // completed
+      wait = false;
+    }
+
+    else if (userInput == 3) { // found
+      // found()
+      strokes = 0;
+    }
+
+    else if (userInput == 4) {
+      //notFound()
+    }
+  }
 }
 
-void clearQueue(){
+void clearQueue() {
+  done = false;
+  function = false;
   mainCt = 0;
   funcCt = 0;
   for (int i = 0; i < rows; i++) { //clear the queues
@@ -95,41 +130,50 @@ void clearQueue(){
   }
 }
 
-String sendCommand() {
-
-    String cmd = commandLookup[mainQueue[queueCt]];
+void sendCommand() {
+  String cmd;
+  if (!function) {  //if we're in the main queue
+    cmd = commandLookup[mainQueue[mainCt]]; //get the byte to send to the rover
+    mainCt++;
     if (cmd == "FUNC") {
-      sendFuncComands();
+      function = true;
+      funcCt = 0;
+      if (mainCt >= rows) {
+        done = true;            //if a user calls a function on the last main queue slot,
+      }                         //stop sending commands at the end of the function queue
+      else {
+        Serial.println(cmd);
+      }
     }
-    else if (cmd != " ") {
-      Serial.println(cmd);
+  }
+  if (function) { // if we're in the function queue
+    cmd = commandLookup[funcQueue[funcCt]];
+    funcCt++;
+    if (funcCt >= rows) { // if we are at the end of the queue
+      function = false;   // don't look for a function command next time.
+      funcCt = 0;
+    }
+    if (cmd != "FUNC" && cmd != " ") {
+      Serial.println(cmd);  //send it to the rover
     }
   }
 }
-void sendFuncComands() {
-  for (int funcCt = 0; funcCt < rows; funcCt++) {
-    String cmd = commandLookup[funcQueue[funcCt]];
-    if (cmd != "FUNC" && cmd != " ") { //ignore recursive functions and unknown commands
-      Serial.println(cmd);
-      waitForResponse();
-    }
-  }
-}
+
 
 void waitForResponse() {
   while (!Serial.available()) {
   }
-  if(Serial.available()){
+  if (Serial.available()) {
     uint8_t inByte = Serial.read();
-    if(inByte = "ready"){
+    if (inByte = "ready") {
       return;
     }
-    else if (inByte = "found"){
+    else if (inByte = "found") {
       resetCounter();
     }
   }
 }
-void resetCounter(){
+void resetCounter() {
   strokes = 0;
 }
 
@@ -143,7 +187,7 @@ void shiftIn() {
   //set it to 0 to transmit data serially
   digitalWrite(latchPin, 0);
 
- 
+
   //we will be holding the clock pin high at the
   //end of each time through the for loop
 
@@ -153,6 +197,7 @@ void shiftIn() {
   //of the next bit in its serial information flow.
 
   uint8_t tempByte = 0;
+  uint8_t rightCompare = 15; //binary 00001111
 
   for (int j = 0; j < rows; j += 2) {
     for (int i = 0; i < 8; i ++) {
@@ -175,14 +220,14 @@ void shiftIn() {
     for (int i = 0; i < 8; i ++) {
       digitalWrite(clockPin, 0);
       delayMicroseconds(2);
-      boolean tempBool = digitalRead(dataPin);
-      bitWrite(tempByte, i, tempBool);
+      boolean tempBit = digitalRead(dataPin);
+      bitWrite(tempByte, i, tempBit);
       digitalWrite(clockPin, 1);
     }
     uint8_t rightNibble = tempByte & rightCompare;   // bitwise AND with 00001111 to extract the second nibble into its own byte
     uint8_t leftNibble = tempByte >> 4;              // bitshift right to extract the first nibble into its own byte
     funcQueue[j] = rightNibble;
-    funcQueue[j + 1] = leftNibble;    
+    funcQueue[j + 1] = leftNibble;
     if (debug) {
       Serial.println (leftNibble, BIN);
       Serial.println(rightNibble, BIN);
